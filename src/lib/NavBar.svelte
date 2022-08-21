d196e853294c2020ce0808711cfd6978a6251b03
Michal Petrik <misopetrik@gmail.com>
2022-08-21 21:19:44 +0200

    ✨ Add button/tooltip transitions (#4)

--- src/lib/NavBar.svelte
+++ src/lib/NavBar.svelte
@@ -1,6 +1,13 @@
 <script lang="ts">
   import NavBarButton from './NavBarButton.svelte';
 
+  const icons = [
+    { icon: 'home', tooltip: 'Home' },
+    { icon: 'briefcase', tooltip: 'Work' },
+    { icon: 'user-graduate', tooltip: 'Education' },
+    { icon: 'laptop', tooltip: 'Projects' },
+    { icon: 'envelope', tooltip: 'Contact' },
+  ];
   let selectedIcon: string = 'home';
 
   const toggleActive = (icon) => (selectedIcon = icon);
@@ -9,7 +16,12 @@
 <div
   class="fixed top-[50%] right-8 flex flex-col items-center justify-center -translate-y-[50%] gap-5"
 >
-  {#each ['home', 'briefcase', 'user-graduate', 'laptop', 'envelope'] as icon}
-    <NavBarButton {icon} {toggleActive} active={icon === selectedIcon} />
+  {#each icons as { icon, tooltip }}
+    <NavBarButton
+      {icon}
+      {tooltip}
+      {toggleActive}
+      active={icon === selectedIcon}
+    />
   {/each}
 </div>
