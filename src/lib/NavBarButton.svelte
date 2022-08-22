05894ba3fb8a6276e2a99ca1d8bf6ea837f046ae
vikingviolinist <misopetrik@gmail.com>
2022-08-22 16:42:14 +0200

    🎉 Add button effects

--- src/lib/NavBarButton.svelte
+++ src/lib/NavBarButton.svelte
@@ -7,8 +7,8 @@
 
 <button
   on:click={toggleActive(icon)}
-  class="control p-4 cursor-pointer w-14 h-14 rounded-3xl hover:bg-secondary hover:rounded-xl
-  transition-all duration-300 ease-linear group
+  class="control p-4 cursor-pointer w-10 lg:w-12 h-10 lg:h-12 rounded-3xl hover:bg-secondary hover:rounded-xl
+  transition-all duration-300 ease-linear group flex justify-center items-center
   shadow-[0_3px_15px_rgba(0,0,0,0.3);] {active ? 'bg-secondary' : 'bg-gray-4'}"
 >
   <i
@@ -16,7 +16,7 @@
         {active ? 'text-white' : 'text-gray-2 dark:text-gray-400'}"
   />
   <span
-    class="absolute w-auto p-2 m-2 -translate-y-2  min-w-max right-16 rounded-md shadow-md
+    class="hidden lg:block absolute w-auto p-2 m-2 min-w-max right-14 rounded-md shadow-md
       text-white bg-gray-4 text-xs font-bold
       transition-all duration-300 scale-0 origin-right group-hover:scale-100"
     >{tooltip}</span
