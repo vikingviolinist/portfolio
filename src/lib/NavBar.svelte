c3f9a5a4b7795b5feb83cda71e1749845e3a4cb7
vikingviolinist <misopetrik@gmail.com>
2022-08-28 18:06:05 +0200

    💄 Change nav button styling

--- src/lib/NavBar.svelte
+++ src/lib/NavBar.svelte
@@ -14,7 +14,7 @@
 </script>
 
 <div
-  class="fixed left-[50%] lg:left-auto bottom-5 lg:top-[50%] lg:right-3 -translate-x-[50%]  lg:-translate-y-[50%] flex lg:flex-col items-center justify-center gap-3 lg:gap-5
+  class="fixed left-[50%] lg:left-auto bottom-5 lg:top-[50%] lg:right-3 -translate-x-[50%]  lg:-translate-y-[50%] flex lg:flex-col items-center justify-center gap-4 lg:gap-5
         transition-all duration-300 ease-in-out"
 >
   {#each icons as { icon, tooltip }}
--- src/lib/NavBarButton.svelte
+++ src/lib/NavBarButton.svelte
@@ -7,7 +7,7 @@
 
 <button
   on:click={toggleActive(icon)}
-  class="control p-4 cursor-pointer w-10 lg:w-12 h-10 lg:h-12 rounded-3xl hover:bg-secondary hover:rounded-xl
+  class="control p-6 cursor-pointer w-10 lg:w-12 h-10 lg:h-12 rounded-3xl hover:bg-secondary hover:rounded-xl
   transition-all duration-300 ease-linear group flex justify-center items-center
   shadow-[0_3px_15px_rgba(0,0,0,0.3);] {active ? 'bg-secondary' : 'bg-gray-4'}"
 >
