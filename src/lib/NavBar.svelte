cc635e901f5c0080666ee55d069d6170e6bc6cd8
vikingviolinist <misopetrik@gmail.com>
2022-09-07 17:49:40 +0200

    💄 Fix z-index for buttons

--- src/lib/NavBar.svelte
+++ src/lib/NavBar.svelte
@@ -8,7 +8,7 @@
 
 <div
   class="fixed left-[50%] lg:left-auto bottom-5 lg:top-[50%] lg:right-3 -translate-x-[50%]  lg:-translate-y-[50%] flex lg:flex-col items-center justify-center gap-4 lg:gap-5
-        transition-all duration-300 ease-in-out"
+        transition-all duration-300 ease-in-out z-10"
 >
   {#each sections as { icon, tooltip }}
     <NavBarButton
--- src/lib/ThemeToggle.svelte
+++ src/lib/ThemeToggle.svelte
@@ -15,7 +15,7 @@
 
 <button
   on:mousedown={toggleTheme}
-  class="active:-translate-y-2 ease-in-out duration-100 theme-btn top-[5%] right-[3%] w-12 h-12 rounded-full border-none fixed bg-gray-4 cursor-pointer shadow-lg"
+  class="active:-translate-y-2 ease-in-out duration-100 theme-btn top-[5%] right-[3%] w-12 h-12 rounded-full border-none fixed bg-gray-4 cursor-pointer shadow-lg z-10"
 >
   <i
     class="theme-icon fas text-gray-2 text-2xl pointer-events-none {prefersDarkTheme
