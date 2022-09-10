b4cba319040aaedf5f3c20abdf0fc78eb5836407
vikingviolinist <misopetrik@gmail.com>
2022-09-10 15:26:48 +0200

    🏷  Make interface reusable

--- src/lib/TimelineItem.svelte
+++ src/lib/TimelineItem.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
-  import type { Job } from '../interfaces/Job';
+  import type { TimelineItem } from '../interfaces/TimelineItem';
 
-  export let job: Job;
+  export let job: TimelineItem;
 </script>
 
 <div class="relative pl-12 border-l-2 border-gray-5">
@@ -13,11 +13,11 @@
   <p
     class="text-gray-2 bg-gray-5 py-1 px-2 rounded-2xl inline-block text-sm font-medium"
   >
-    {job.from} - {job.to ?? 'i dag'}
+    {job.from} - {job.to}
   </p>
   <h5 class="py-4 text-xl font-semibold">
     {job.title}
-    <span class="text-gray-2 font-medium text-xl">- {job.company}</span>
+    <span class="text-gray-2 font-medium text-xl">- {job.institution}</span>
   </h5>
   <p class="text-gray-2 text-justify">
     {job.description}
