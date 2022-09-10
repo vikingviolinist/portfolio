da596564eeeb8d23b2b9b40e01991848bd3bc8f9
vikingviolinist <misopetrik@gmail.com>
2022-09-10 18:54:07 +0200

    🚸 Disable tooltip pointer events

--- src/lib/Tooltip.svelte
+++ src/lib/Tooltip.svelte
@@ -6,7 +6,7 @@
 
 <span
   class="hidden lg:block absolute w-auto p-2 m-2 min-w-max rounded-md shadow-md
-      text-white bg-gray-4 text-xs font-bold
+      text-white bg-gray-4 text-xs font-bold pointer-events-none
       transition-all duration-300 scale-0 origin-{origin} group-hover:scale-100 {position}"
   >{tooltip}</span
 >
