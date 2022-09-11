acd66f0a8b1b020cf92078289867a3d6f0dac167
Michal Petrik <misopetrik@gmail.com>
2022-09-11 20:33:37 +0200

    🎨 Dynamic images (#23)
    * 🚸 Disable tooltip pointer events
    
    * ➕ Install unsplash-js
    
    * 🎨 Implement dynamic images for projects

--- src/lib/Projects.svelte
+++ src/lib/Projects.svelte
@@ -1,19 +1,28 @@
 <script lang="ts">
+  import { createApi } from 'unsplash-js';
   import { technologies } from '../technologies';
   import DevIcon from './DevIcon.svelte';
   import Section from './Section.svelte';
   import Project from './Project.svelte';
   export let active: boolean;
+
+  const accessKey = '2jacHDYybwpIjozi70jBxq_FdkhUhap37l99qairwI0';
+  const unsplash = createApi({ accessKey });
+  const promise = unsplash.photos.getRandom({
+    query: 'programming',
+    count: 6,
+  });
 </script>
 
 <Section {active} type="Prosjekter">
   <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mb-20">
-    <Project path="./images/port1.jpeg" />
-    <Project path="./images/port2.jpeg" />
-    <Project path="./images/port3.jpeg" />
-    <Project path="./images/port5.jpeg" />
-    <Project path="./images/port6.jpeg" />
-    <Project path="./images/port7.jpeg" />
+    {#await promise}
+      <p>Loading...</p>
+    {:then { response }}
+      {#each response as image}
+        <Project path={image.urls.small_s3} />
+      {/each}
+    {/await}
   </div>
   <div class="dev-icons flex justify-center flex-wrap gap-8 pb-20">
     {#each technologies as technology}
