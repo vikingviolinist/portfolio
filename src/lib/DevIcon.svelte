5952f175ee800e147eab72c7231f33097535dab8
vikingviolinist <misopetrik@gmail.com>
2022-09-13 22:00:17 +0200

    ♻️  Refactor DevIcon

--- src/lib/DevIcon.svelte
+++ src/lib/DevIcon.svelte
@@ -1,87 +1,8 @@
 <script lang="ts">
-  import go from './icons/Go.svelte';
-  import react from './icons/React.svelte';
-  import svelte from './icons/Svelte.svelte';
-  import angularjs from './icons/Angular.svelte';
-  import nodejs from './icons/Nodejs.svelte';
-  import javascript from './icons/Javascript.svelte';
-  import typescript from './icons/Typescript.svelte';
-  import html5 from './icons/Html.svelte';
-  import css3 from './icons/Css.svelte';
-  import tailwindcss from './icons/tailwindcss.svelte';
-  import graphql from './icons/graphql.svelte';
-  import jest from './icons/jest.svelte';
-  import mysql from './icons/mysql.svelte';
-  import postgresql from './icons/postgresql.svelte';
-  import mongodb from './icons/mongodb.svelte';
-  import docker from './icons/docker.svelte';
-  import amazonwebservices from './icons/amazonwebservices.svelte';
-  import github from './icons/github.svelte';
-  import git from './icons/git.svelte';
-  import babel from './icons/babel.svelte';
-  import bash from './icons/bash.svelte';
-  import bootstrap from './icons/bootstrap.svelte';
-  import circleci from './icons/circleci.svelte';
-  import django from './icons/django.svelte';
-  import eslint from './icons/eslint.svelte';
-  import express from './icons/express.svelte';
-  import figma from './icons/figma.svelte';
-  import jira from './icons/jira.svelte';
-  import linux from './icons/linux.svelte';
-  import npm from './icons/npm.svelte';
-  import python from './icons/python.svelte';
-  import redux from './icons/redux.svelte';
-  import sass from './icons/sass.svelte';
-  import ubuntu from './icons/ubuntu.svelte';
-  import trello from './icons/trello.svelte';
-  import webpack from './icons/webpack.svelte';
-
   import Tooltip from './Tooltip.svelte';
 
-  export let technology: string;
-  const icons = {
-    go,
-    react,
-    svelte,
-    // sveltekit,
-    // netlify,
-    angularjs,
-    nodejs,
-    javascript,
-    typescript,
-    html5,
-    css3,
-    tailwindcss,
-    jest,
-    mysql,
-    graphql,
-    postgresql,
-    mongodb,
-    docker,
-    amazonwebservices,
-    github,
-    git,
-    babel,
-    bash,
-    bootstrap,
-    circleci,
-    django,
-    eslint,
-    express,
-    figma,
-    jira,
-    linux,
-    npm,
-    python,
-    redux,
-    sass,
-    ubuntu,
-    trello,
-    webpack,
-    // vite
-  };
-
-  let component = icons[technology];
+  export let component;
+  export let tooltip: string;
 </script>
 
 <div class="group relative flex justify-center">
@@ -91,5 +12,5 @@
   >
     <svelte:component this={component} />
   </div>
-  <Tooltip position="bottom-16" origin="bottom" tooltip={technology} />
+  <Tooltip position="bottom-16" origin="bottom" {tooltip} />
 </div>
