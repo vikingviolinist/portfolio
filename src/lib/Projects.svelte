728cf7ffcef9b97755b4c9dd7b14b95619c9bbe3
vikingviolinist <misopetrik@gmail.com>
2022-09-14 15:15:11 +0200

    ♻️  Import icons as module

--- src/lib/Projects.svelte
+++ src/lib/Projects.svelte
@@ -1,95 +1,12 @@
 <script lang="ts">
-  import go from './icons/go.svelte';
-  import react from './icons/react.svelte';
-  import svelte from './icons/svelte.svelte';
-  import angular from './icons/angular.svelte';
-  import nodejs from './icons/nodejs.svelte';
-  import javascript from './icons/javascript.svelte';
-  import typescript from './icons/typescript.svelte';
-  import html from './icons/html.svelte';
-  import css from './icons/css.svelte';
-  import tailwindcss from './icons/tailwindcss.svelte';
-  import graphql from './icons/graphql.svelte';
-  import jest from './icons/jest.svelte';
-  import mysql from './icons/mysql.svelte';
-  import postgresql from './icons/postgresql.svelte';
-  import mongodb from './icons/mongodb.svelte';
-  import docker from './icons/docker.svelte';
-  import aws from './icons/aws.svelte';
-  import github from './icons/github.svelte';
-  import git from './icons/git.svelte';
-  import babel from './icons/babel.svelte';
-  import bash from './icons/bash.svelte';
-  import bootstrap from './icons/bootstrap.svelte';
-  import circleci from './icons/circleci.svelte';
-  import django from './icons/django.svelte';
-  import eslint from './icons/eslint.svelte';
-  import express from './icons/express.svelte';
-  import figma from './icons/figma.svelte';
-  import jira from './icons/jira.svelte';
-  import linux from './icons/linux.svelte';
-  import npm from './icons/npm.svelte';
-  import python from './icons/python.svelte';
-  import redux from './icons/redux.svelte';
-  import sass from './icons/sass.svelte';
-  import ubuntu from './icons/ubuntu.svelte';
-  import trello from './icons/trello.svelte';
-  import webpack from './icons/webpack.svelte';
-  import vite from './icons/vite.svelte';
-  import parcel from './icons/parcel.svelte';
-  import netlify from './icons/netlify.svelte';
-  import prettier from './icons/prettier.svelte';
-
-  import { technologies } from '../technologies';
+  import * as icons from './icons';
   import DevIcon from './DevIcon.svelte';
   import Section from './Section.svelte';
   import Project from './Project.svelte';
+
   export let active: boolean;
   export let projects;
   export let images;
-
-  const icons = {
-    go,
-    react,
-    svelte,
-    netlify,
-    angular,
-    nodejs,
-    javascript,
-    typescript,
-    html,
-    css,
-    tailwindcss,
-    jest,
-    mysql,
-    graphql,
-    postgresql,
-    mongodb,
-    docker,
-    aws,
-    github,
-    git,
-    babel,
-    bash,
-    bootstrap,
-    circleci,
-    django,
-    eslint,
-    express,
-    figma,
-    jira,
-    linux,
-    npm,
-    python,
-    redux,
-    sass,
-    ubuntu,
-    trello,
-    webpack,
-    vite,
-    parcel,
-    prettier,
-  };
 </script>
 
 <Section {active} type="Prosjekter">
@@ -107,8 +24,8 @@
     {/if}
   </div>
   <div class="dev-icons flex justify-center flex-wrap gap-8 pb-20">
-    {#each technologies as technology}
-      <DevIcon component={icons[technology]} tooltip={technology} />
+    {#each Object.keys(icons) as icon}
+      <DevIcon component={icons[icon]} tooltip={icon} />
     {/each}
   </div>
 </Section>
