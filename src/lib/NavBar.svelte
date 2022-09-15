d6b13376685443e9ac550d59a518dc5812a4fff5
vikingviolinist <misopetrik@gmail.com>
2022-09-15 19:53:10 +0200

    ⚡ Make handler conditional

--- src/lib/NavBar.svelte
+++ src/lib/NavBar.svelte
@@ -8,12 +8,12 @@
 </script>
 
 <div
-  class="fixed left-[50%] lg:left-auto bottom-5 lg:top-[50%] lg:right-3 -translate-x-[50%]  lg:-translate-y-[50%] flex lg:flex-col items-center justify-center gap-4 lg:gap-5
+  class="fixed left-[50%] lg:left-auto bottom-5 lg:top-[50%] lg:right-3 -translate-x-[50%] lg:-translate-y-[50%] flex lg:flex-col items-center justify-center gap-4 lg:gap-5
         transition-all duration-300 ease-in-out z-10"
 >
   {#each sections as { icon, tooltip }}
     <NavBarButton
-      {loadProjects}
+      loadProjects={icon === 'laptop' && loadProjects}
       active={icon === selectedIcon}
       {toggleActive}
       {tooltip}
