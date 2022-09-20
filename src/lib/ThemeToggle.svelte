b962281263d681bc41c4b45b9efb4626e043c428
vikingviolinist <misopetrik@gmail.com>
2022-09-20 21:56:34 +0200

    🎨 Simplify theme toggle

--- src/lib/ThemeToggle.svelte
+++ src/lib/ThemeToggle.svelte
@@ -1,30 +1,9 @@
 <script lang="ts">
-	import { browser } from '$app/environment';
+	import theme from '../theme';
 
-	let darkMode: boolean;
-
-	function toggleTheme() {
-		darkMode = !darkMode;
-
-		localStorage.setItem('theme', darkMode ? 'dark' : 'light');
-
-		darkMode
-			? document.documentElement.classList.add('dark')
-			: document.documentElement.classList.remove('dark');
-	}
-
-	if (browser) {
-		if (
-			localStorage.theme === 'dark' ||
-			(!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)
-		) {
-			document.documentElement.classList.add('dark');
-			darkMode = true;
-		} else {
-			document.documentElement.classList.remove('dark');
-			darkMode = false;
-		}
-	}
+	const toggleTheme = () => {
+		theme.set($theme === 'dark' ? 'light' : 'dark');
+	};
 </script>
 
 <button
@@ -32,7 +11,7 @@
 	class="active:-translate-y-2 ease-in-out duration-100 theme-btn top-[5%] right-[3%] w-12 h-12 rounded-full border-none fixed bg-gray-4 cursor-pointer shadow-lg z-10"
 >
 	<i
-		class="theme-icon fas text-gray-2 text-2xl pointer-events-none {darkMode
+		class="theme-icon fas text-gray-2 text-2xl pointer-events-none {$theme === 'dark'
 			? 'fa-sun'
 			: 'fa-moon'}"
 	/>
--- src/theme.ts
+++ src/theme.ts
@@ -1,32 +1,29 @@
 import { browser } from '$app/environment';
 import { writable } from 'svelte/store';
 
-type Theme = 'dark' | 'light';
-
-const defaultTheme: Theme = 'dark';
-const prefersDarkTheme = browser ?? window.matchMedia('(prefers-color-scheme: dark)').matches;
-const hasThemeSet = browser ? 'theme' in window.localStorage : false;
-let initialValue: Theme;
+let defaultTheme: 'dark' | 'light' = 'dark';
 
 if (browser) {
-	initialValue =
-		(window.localStorage.getItem('theme') as Theme) ||
-		((!hasThemeSet && prefersDarkTheme) ?? defaultTheme);
-} else {
-	initialValue = defaultTheme;
+	if (
+		localStorage.theme === 'dark' ||
+		(!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)
+	) {
+		defaultTheme = 'dark';
+	} else {
+		defaultTheme = 'light';
+	}
 }
 
-if (browser && initialValue === 'dark') {
-	document.documentElement.classList.add('dark');
-} else {
-	document.documentElement.classList.remove('dark');
-}
-
-const theme = writable<string>(initialValue);
+const theme = writable<string>(defaultTheme);
 
 theme.subscribe((theme) => {
 	if (browser) {
 		window.localStorage.setItem('theme', theme);
+		if (theme === 'dark') {
+			document.documentElement.classList.add('dark');
+		} else {
+			document.documentElement.classList.remove('dark');
+		}
 	}
 });
 
