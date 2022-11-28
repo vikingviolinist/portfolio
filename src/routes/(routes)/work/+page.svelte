65d9c9eba71a9c776a5e30bbfef1a6221af4030d
vikingviolinist <misopetrik@gmail.com>
2022-11-28 17:47:51 +0100

    💩 Fail deploy test

--- src/routes/(routes)/work/+page.svelte
+++ src/routes/(routes)/work/+page.svelte
@@ -7,7 +7,7 @@
 
 <Header title="Min Arbeidserfaring" />
 <div class="grid grid-cols-1 lg:grid-cols-2 gap-8 pb-12 pl-4">
-	{#each jobs as item}
-		<TimelineItem {item} />
+	{#each jobs as job}
+		<TimelineItem {job} />
 	{/each}
 </div>
